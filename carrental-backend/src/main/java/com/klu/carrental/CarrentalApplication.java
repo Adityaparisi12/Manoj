191f5f6a4eeebb54824c85328400dcae66e2cf83
Aditya <adityaparisi12@gmail.com>
2025-11-08 12:36:49 +0530

    updated files

--- carrental-backend/src/main/java/com/klu/carrental/CarrentalApplication.java
+++ carrental-backend/src/main/java/com/klu/carrental/CarrentalApplication.java
@@ -8,6 +8,7 @@ public class CarrentalApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CarrentalApplication.class, args);
+		System.out.println("Apllication is running");
 	}
 
 }
